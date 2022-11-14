f63d82bbc58d8b46a88cb2cd5b4a072a85ffd6c3
CreeperGuy72192 <65190117+CreeperGuy72192@users.noreply.github.com>
2022-11-13 19:39:52 -0500

    Add files via upload

--- js/desktop.js
+++ js/desktop.js
@@ -10,13 +10,26 @@ window.onload = () => {
     var s = d.getSeconds();
     var m = d.getMinutes();
     var h = d.getHours();
-    span.textContent = 
+    span.textContent =
       // uncomment below for normal time
       // ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
-      (h > 12 ? h - 12 : h) + ":" + ("0" + m).substr(-2) + ' ' + (h > 11 ? 'PM' : 'AM');
+      (h > 12 ? h - 12 : (h < 1 ? '12' : h)) + ":" + ("0" + m).substr(-2) + ' ' + (h > 11 ? 'PM' : 'AM');
   }
   time();
   setInterval(time, 1000);
+
+  // Create and display the settings window
+  let wbSettings = WinBox({
+    title: 'System Settings',
+    html: document.querySelector('[data-wintemplate="settings"]').innerHTML,
+    class: ['no-full'],
+    x: "center",
+    y: "center",
+    minwidth: '700',
+    minheight: '460',
+    width: '895',
+    height: '580',
+    
+  });
+  
 }
-
-
